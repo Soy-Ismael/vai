3e53d5f1705043b4e9d43c93c92e05a9f796d46d
Soy-ismael <ismaelfelix547@gmail.com>
2023-12-24 16:55:02 -0300

    Publicación de estructura de trabajo y repartición de la carga

--- dev/vai.py
+++ dev/vai.py
@@ -1,1 +1,19 @@
+# ESTRUCTURA Y REPARTICION DE TRABAJO DEL ASISTENTE
+# *     Jared y Jairon
+#todo 1 - Grabar voz del usuario 🎤
+#todo 2 - Convertir lo que dijo en texto 🖋
+# 
+# *     Xaviel e Ismael
+#todo 3 - Procesar la intención del usuario ❓ (modelo de IA)
+#todo 4 - Ejecutar la acción deseada 👨‍🏭
+# 
+# ?      Proximamente
+#todo 5 - Preparar respuesta (en texto) 💬
+#todo 6 - Convertir en audio y reproducir 🦻
+
+
+
 import speech_recognition as sr
+
+# Variables de entorno
+name = "Jarvis" # Nombre por el que se llamara al asistente (para desarrollar más tarde)
