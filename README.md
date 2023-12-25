b45b1d09c3af0dd9bcd07a67d9eb73a3264ac88e
Soy-ismael <ismaelfelix547@gmail.com>
2023-12-25 16:34:48 -0300

    Desarrollo del modulo interpretación del lenguaje y ejecutar accion del proyecto vai

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 El proyecto VAI será un asistente virtual potenciado con IA con la capacidad de cambiar el nombre de llamada por uno cómodo para el usuario. VAI será desarrollado inicialmente en Python y tendrá la capacidad de ejecutarse en todos los sistemas operativos. Su objetivo principal será el de ofrecer información y ayudar con tareas ambiguas al usuario.
 
 ## Consideraciones Tecnicas
+    Deben crear su API de OPEN AI y colocarla en el archivo .env.example atendiente a las reglas de lugar (especificadas en el archivo)
+
 ### Librerias a instalar hasta el momento
 
 * pip install SpeechRecognition
+* pip install python-dotenv
+
+Open AI - Chat GPT
+* pip install --upgrade openai
+Google - Gemini Pro
+* pip install -q -U google-generativeai
+<!-- * pip install google-colab -->
--- dev/vai.py
+++ dev/vai.py
@@ -11,9 +11,102 @@
 #todo 5 - Preparar respuesta (en texto) 💬
 #todo 6 - Convertir en audio y reproducir 🦻
 
-
-
+# Importaciones 
 import speech_recognition as sr
+from dotenv import load_dotenv
+import os
+# Open AI - Chat Gpt
+from openai import OpenAI
+# Google - Gemini
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+# Used to securely store your API key
+# from google.colab import userdata
+
+# from IPython.display import display
+# from IPython.display import Markdown
+
+
+# def to_markdown(text):
+#     text = text.replace('•', '  *')
+#     return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+
+name = "Jarvis" # Nombre por el que se llamara al asistente (para desarrollar más tarde)
+
+
+
+
+
+
+#* =========================
+#* PARTE DE Ismael Y Xaviel - con open AI
+#* =========================
+# Cargar las variables de entorno (variables contenidas en archivos .env)
+load_dotenv()
+
+# Almanecar variable de entorno en una variable de python con dotenv
+# open_ai_api = os.getenv('OPENAI_API_KEY')
+# print(open_ai_api)
 
 # Variables de entorno
-name = "Jarvis" # Nombre por el que se llamara al asistente (para desarrollar más tarde)
+
+#* Chat GPT
+client = OpenAI()
+# print(client.api_key)
+
+try:
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "Eres un asistente virtual que habla en prosa y responde de manera cortez."},
+        {"role": "user", "content": "Dime como puedo crear una funcion en python."}
+    ])
+    print(completion.choices[0].message)
+except Exception as err:
+    print(err)
+
+
+#* GEMINI Pro
+# google_api_key = os.getenv('GOOGLE_API_KEY')
+
+# Or use `os.getenv('GOOGLE_API_KEY')` to fetch an environment variable.
+# google_api_key = userdata.get('GOOGLE_API_KEY')
+
+# print(google_api_key) 
+# print(type(google_api_key))
+
+# model = genai.GenerativeModel('gemini-pro')
+# chat = model.start_chat(history=[])
+
+# response = chat.send_message("Dime como puedo crear una funcion en python.")
+# to_markdown(response.text)
+# print(response.text)
+# print(chat.history)
+
+# model = genai.GenerativeModel('gemini-pro')
+# chat = model.start_chat(history=[])
+# print(model.count_tokens)
+# print(chat.model)
+
+# response = chat.send_message(
+#     "Pretend you\'re a snowman and stay in character for each response.")
+# # print(response.text)
+
+# response = chat.send_message(
+#     "What\'s your favorite season of the year?")
+# # print(response.text)
+
+
+
+
+# google_api_key = os.getenv('GOOGLE_API_KEY')
+# genai.configure(api_key= google_api_key)
+
+# model = genai.get_model("tunedModels/gemini-pro") # Elige el modelo gemini-pro
+# prompt = "¿Qué es la inteligencia artificial?" # Define tu entrada de texto
+# response = model.generateContent(prompt=prompt) # Genera una respuesta de texto
+# print(response) # Imprime la respuesta
