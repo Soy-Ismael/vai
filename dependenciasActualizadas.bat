75c799496ddd32e7117cfe618ebdac281fd93aac
Soy-Ismael <ismaelfelix547@gmail.com>
2024-03-12 19:12:36 -0400

    Subida de dependencias actualizadas a backup

---
+++ dependenciasActualizadas.bat
@@ -0,0 +1,81 @@
+@echo off
+echo Automated Installer
+
+title Actualizando pip
+color 8
+python.exe -m pip install --upgrade pip
+echo TERMINADO
+cls
+
+title Instalando SpeechRecognition
+color 5
+pip install SpeechRecognition
+echo TERMINADO
+cls
+
+title Instalando PyAudio
+color 6
+pip install PyAudio
+echo TERMINADO
+cls
+
+title Instalando dotenv
+color d
+pip install python-dotenv
+echo TERMINADO
+cls
+
+title Instalando setuptools py-3.12
+color b
+python -m pip install setuptools
+echo TERMINADO
+cls
+
+title Instalando distutils644 (opt)
+color c
+pip install distutils644
+echo TERMINADO
+cls
+
+title Instalando OpenAI
+color 2
+pip install --upgrade openai
+echo TERMINADO
+cls
+
+title Instalando pyttsx3
+color 9
+pip install pyttsx3
+echo TERMINADO
+cls
+
+title Instalando pywhatkit
+color a
+pip install pywhatkit
+echo TERMINADO
+cls
+
+title Instalando spoty
+color 2
+pip install spoty
+echo TERMINADO
+cls
+
+title Instalando pyjokes
+color 5
+pip3 install pyjokes
+echo TERMINADO
+cls
+
+@REM text-to-speech de openAI
+@REM title Instalando pathlib
+@REM color c
+@REM pip install pathlib
+@REM echo TERMINADO
+@REM cls
+
+@REM https://platform.openai.com/docs/guides/text-to-speech
+
+title Terminado
+msg * "Instalacion de dependencias completada con exito"
+
---
+++ dev/pruebas.py
@@ -0,0 +1,5 @@
+from pyfiglet import Figlet
+
+custom_fig = Figlet(font='avatar')
+
+print(custom_fig.renderText('PROM2023-2024'))
