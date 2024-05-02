8703710ed002d16c0dbe13edd0469f49d5958acf
Soy-Ismael <ismaelfelix547@gmail.com>
2024-05-02 10:44:16 -0400

    Gran actualización, el asistente ahora es capaz de realizar reportes de archivos de excel con un formato especifico a peticion audible del usuario, ahora el asistente puede hablar todos los idiamas, con muchos acentos distintos si se tiene una clave api de portal.azure, actualización de .gitignore, las dependencias que se instalan y el readme.

--- dev/va.py
+++ dev/va.py
@@ -30,7 +30,6 @@ else:
 import speech_recognition as sr # Módulo para reconocer audio y convertir a texto (STT)
 import pyttsx3 # Módulo para convertir de texto a audio (TTS)
 from dotenv import get_key # Módulo para cargar api-key en archivo .env
-from dotenv import get_key # Módulo para cargar api-key en archivo .env
 import datetime # Módulo para manejar la hora
 import pywhatkit # Módulo para enviar mensajes de whatapp y abrir contenido en youtube (es un kit)
 import random #Nuevo módulo para generar números aleatorios
@@ -46,6 +45,7 @@ from days import getDaysAgo
 # import spoty # Módulo para reproducir contenido en spotify (no esta en uso actualmente)
 # from sys import exit #Para trabajar con sys.exit() en caso de ser necesario
 from banner import figlet_banner # Nuevo módulo local para imprimir banner de los desarrolladores
+from report import create_report # Módulo para crear un reporte de excel de un archivo especifico
 # from voice_synthesizer import synthesize_to_speaker # Módulo local creado para tts de microsoft (más voces y calidad que pyttsx3, no depende de voces en el ordenador)
 #* Open AI - Chat Gpt
 from openai import OpenAI # Módulo para inteligencia artificial
@@ -157,7 +157,7 @@ rec = sr.Recognizer()
 
 # 2300 tiene problemas para entender
 # rec.energy_threshold = 2900
-#* Función para escuchar la petición del usuario, se puede invocar durante la ejecución del programa, lo que permite que el asistente pueda volver a escuchar en cualquier punto del programa con solo invocar la función
+#* Función para escuchar la petición del usuario, se puede invocar durante la ejecución del programa, lo que permite que el asistente pueda volver a escuchar en cualquier punto del programa con solo invocar la función.
 def listen():
     # Acceder al microfono del dispositivo
     try:
@@ -478,6 +478,12 @@ def run(text:str = '', status=True):
         return True
 
 
+    elif 'realiza' in text['text'] and 'reporte' in text['text']:
+        print('Creando reporte')
+        create_report()
+
+
+
     elif 'envía' in text['text']:
         text = text['text'].replace('envía', '')
 
@@ -626,6 +632,10 @@ def run(text:str = '', status=True):
         print(date)
         talk(date)
 
+    elif 'más' in text['text']:
+        # código para obtener los numeros del texto de entrada, sumarlos y devolverlos
+        pass
+
     #! IMPORTANTE
     #* Con global le indico que la variable text sera global en lugar de local, como la variable text existe, entonces estoy indicando que quiero utilizar la variable global y no crear una variable nueva dentro de la función, esto deberia solucionar el error de "UnboundLocalError" 
     # global text
@@ -712,7 +722,8 @@ try:
     # import time
     # if not run():
     #     talk(run_gpt())
-    run('qué día fue hace 5 dias')
+    run('realiza un reporte')
+    pass
 
     #* Implementando funcionalidad para que el asistente se mantenga escuchando
     # run('dime un chiste')
--- dev/voice_synthesizer.py
+++ dev/voice_synthesizer.py
@@ -1,9 +1,19 @@
 import azure.cognitiveservices.speech as speechsdk
 from azure.cognitiveservices.speech.audio import AudioOutputConfig
 from dotenv import get_key
+# from va import talk
+import pyttsx3
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
 
 # función para sintetizar el texto y convertirlo a voz, recibe como parametros: el texto a sintetizar, el idioma y acento con el que va a hablar, el idioma que va a reconocer, la clave api de azure y la región de esa clave api, (tiene valores por defecto cada parametro para una ejecución más limpia de la función)
-def synthesize_to_speaker(text:str, voice:str='es-MX-DaliaNeural', speech_language:str='es-MX', recognition_language:str='es-MX', azure_api_key:str=get_key('AZURE_API_KEY'), region:str='northAmerica'):
+
+def synthesize_to_speaker(text:str='Hola mundo', voice:str='es-MX-DaliaNeural', recognition_language:str='es-MX', azure_api_key:str=get_key('dev/.env',"AZURE_API_KEY"), region:str='eastus'):
     #Find your key and resource region under the 'Keys and Endpoint' tab in your Speech resource in Azure Portal
     # speech_config = speechsdk.SpeechConfig(subscription="<YOUR_ID_SUBSCRIPTION>", region="<YOUR_REGION>",speech_recognition_language="es-MX")
     speech_config = speechsdk.SpeechConfig(subscription=azure_api_key, region=region,speech_recognition_language = recognition_language)
@@ -13,14 +23,19 @@ def synthesize_to_speaker(text:str, voice:str='es-MX-DaliaNeural', speech_langua
 
     # Note: if only language is set, the default voice of that language is chosen.
     # speech_config.speech_synthesis_çlanguage = "es-MX"
-    speech_config.speech_synthesis_language = speech_language
+    speech_config.speech_synthesis_language = voice[0:5]
 
     # The voice setting will overwrite language setting.
     # The voice setting will not overwrite the voice element in input SSML.
-    speech_config.speech_synthesis_voice_name = "es-MX-DaliaNeural"
+    speech_config.speech_synthesis_voice_name = voice
     synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 
-    synthesizer.speak_text_async(text)
+    synthesizer.speak_text(text)
 
 if __name__ == "__main__":
-    synthesize_to_speaker("Hola, ¿como estas? Esta es la voz del sintetizador de Microsoft", azure_api_key='<API_KEY>', region='<API_KEY_REGION>')
+    #* Ejemplo de uso
+    # synthesize_to_speaker("Hola, ¿como estas? Esta es la voz del sintetizador de Microsoft", azure_api_key='<API_KEY>', region='<API_KEY_REGION>')
+
+    # talk(text='Uy parce, ¡dígame cómo puedo ayudarle el día de hoy pues!')
+    # synthesize_to_speaker(text="Hola, ¿como estas? Esta es la voz del sintetizador de Microsoft")
+    pass
