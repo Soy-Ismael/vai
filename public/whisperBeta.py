c5e37f465425148d3ff05aa05e0ec68169919e91
Soy-Ismael <ismaelfelix547@gmail.com>
2024-05-08 22:58:23 -0400

    Publicacion de código en rama public (aun falta limpiar los comentarios

---
+++ public/whisperBeta.py
@@ -0,0 +1,59 @@
+import io
+from pydub import AudioSegment
+import speech_recognition as sr
+import whisper
+import tempfile
+import os
+import pyttsx3
+import pywhatkit
+
+# pip install pydub
+# pip install -U openai-whisper
+# pip install tempfile
+# pip install ffmpeg
+# pip install ffprobe
+
+
+temp_file = tempfile.mkdtemp()
+# save_path = os.path.join(temp_file, 'temp.wav')
+save_path = 'dev/temp.wav'
+
+listener = sr.Recognizer()
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('rate', 145)
+engine.setProperty('voice', voices[0].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+    
+def listen():
+    try:
+        with sr.Microphone() as source:
+            print("Escuchando...")
+            listener.adjust_for_ambient_noise(source)
+            audio = listener.listen(source)
+            data = io.BytesIO(audio.get_wav_data())
+            audio_clip = AudioSegment.from_file(data)
+            audio_clip.export(save_path, format ='wav')
+    except Exception as e:
+        print('Error: ' + str(e))
+    return save_path
+
+def recognize_audio(save_path, model):
+    audio_model = whisper.load_model(model)
+    transcription = audio_model.transcribe(save_path, language='spanish', fp16=False)
+    return transcription['text']
+
+def main(model:str = 'base'):
+        return recognize_audio(listen(), model)
+        # talk(response)
+        # print(response)
+
+    
+if __name__ == "__main__":
+    text_by_whisper = main()
+    print(text_by_whisper)
+    talk(text_by_whisper)
